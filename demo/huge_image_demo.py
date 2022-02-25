4e39a452a64d00c1300ecca2b926234e48e5ef85
jbwang1997 <jbwang1997@gmail.com>
2022-02-25 17:18:14 +0800

    inference huge image

---
+++ demo/huge_image_demo.py
@@ -0,0 +1,58 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from argparse import ArgumentParser
+
+from mmdet.apis import init_detector, show_result_pyplot
+
+from mmrotate.apis import inference_detector_by_patches
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('img', help='Image file')
+    parser.add_argument('config', help='Config file')
+    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument(
+        '--patch_sizes',
+        type=int,
+        nargs='+',
+        default=[1024],
+        help='The sizes of patches')
+    parser.add_argument(
+        '--patch_steps',
+        type=int,
+        nargs='+',
+        default=[824],
+        help='The steps between two patches')
+    parser.add_argument(
+        '--img_ratios',
+        type=float,
+        nargs='+',
+        default=[1.0],
+        help='Image resizing ratios for multi-scale detecting')
+    parser.add_argument(
+        '--merge_iou_thr',
+        type=float,
+        default=0.1,
+        help='IoU threshould for merging results')
+    parser.add_argument(
+        '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument(
+        '--score-thr', type=float, default=0.3, help='bbox score threshold')
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    # build the model from a config file and a checkpoint file
+    model = init_detector(args.config, args.checkpoint, device=args.device)
+    # test a single image
+    result = inference_detector_by_patches(model, args.img, args.patch_sizes,
+                                           args.patch_steps, args.img_ratios,
+                                           args.merge_iou_thr)
+    # show the results
+    show_result_pyplot(model, args.img, result, score_thr=args.score_thr)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ mmrotate/apis/inference.py
@@ -0,0 +1,90 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import mmcv
+import numpy as np
+import torch
+from mmcv.ops import RoIPool
+from mmcv.parallel import collate, scatter
+from mmdet.datasets import replace_ImageToTensor
+from mmdet.datasets.pipelines import Compose
+
+from mmrotate.core import get_multiscale_patch, merge_results, slide_window
+
+
+def inference_detector_by_patches(model,
+                                  img,
+                                  sizes,
+                                  steps,
+                                  ratios,
+                                  merge_iou_thr,
+                                  bs=1):
+    """inference patches with the detector.
+
+    Split huge image(s) into patches and inference them with the detector.
+    Finally, merge patch results on one huge image by nms.
+
+    Args:
+        model (nn.Module): The loaded detector.
+        img (str | ndarray or): Either an image file or loaded image.
+        sizes (list): The sizes of patches.
+        steps (list): The steps between two patches.
+        ratios (list): Image resizing ratios for multi-scale detecting.
+        merge_iou_thr (float): IoU threshold for merging results.
+        bs (int): Batch size, must greater than or equal to 1.
+
+    Returns:
+        If imgs is a list or tuple, the same length list type results
+        will be returned, otherwise return the detection results directly.
+    """
+    assert bs >= 1, 'The batch size must greater than or equal to 1'
+    cfg = model.cfg
+    device = next(model.parameters()).device  # model device
+    cfg = cfg.copy()
+    # set loading pipeline type
+    cfg.data.test.pipeline[0].type = 'LoadPatchFromImage'
+    cfg.data.test.pipeline = replace_ImageToTensor(cfg.data.test.pipeline)
+    test_pipeline = Compose(cfg.data.test.pipeline)
+
+    if not isinstance(img, np.ndarray):
+        img = mmcv.imread(img)
+    height, width = img.shape[:2]
+    sizes, steps = get_multiscale_patch(sizes, steps, ratios)
+    windows = slide_window(width, height, sizes, steps)
+
+    # prepare patch data
+    patch_datas = []
+    for window in windows:
+        data = dict(img=img, win=window.tolist())
+        # build the data pipeline
+        data = test_pipeline(data)
+        patch_datas.append(data)
+
+    results = []
+    start = 0
+    while True:
+        data = patch_datas[start:start + bs]
+        data = collate(data, samples_per_gpu=len(data))
+        # just get the actual data from DataContainer
+        data['img_metas'] = [
+            img_metas.data[0] for img_metas in data['img_metas']
+        ]
+        data['img'] = [img.data[0] for img in data['img']]
+        if next(model.parameters()).is_cuda:
+            # scatter to specified GPU
+            data = scatter(data, [device])[0]
+        else:
+            for m in model.modules():
+                assert not isinstance(
+                    m, RoIPool
+                ), 'CPU inference with RoIPool is not supported currently.'
+
+        # forward the model
+        with torch.no_grad():
+            results.extend(model(return_loss=False, rescale=True, **data))
+
+        if start + bs >= len(patch_datas):
+            break
+        start += bs
+
+    results = merge_results(
+        results, windows[:, :2], iou_thr=merge_iou_thr, device=device)
+    return results
