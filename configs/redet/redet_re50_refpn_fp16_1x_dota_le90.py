2ddc79d29c0e156b4e7d525dc596de3f8ef47246
erenzhou <592267829@qq.com>
2022-03-05 17:36:56 +0800

    fix lint

--- configs/redet/redet_re50_refpn_fp16_1x_dota_le90.py
+++ configs/redet/redet_re50_refpn_fp16_1x_dota_le90.py
@@ -1,5 +1,3 @@
-_base_ = [
-    './redet_re50_refpn_1x_dota_le90.py'
-]
+_base_ = ['./redet_re50_refpn_1x_dota_le90.py']
 
 fp16 = dict(loss_scale='dynamic')
--- configs/roi_trans/roi_trans_r50_fpn_fp16_1x_dota_le90.py
+++ configs/roi_trans/roi_trans_r50_fpn_fp16_1x_dota_le90.py
@@ -1,5 +1,3 @@
-_base_ = [
-    './roi_trans_r50_fpn_1x_dota_le90.py'
-]
+_base_ = ['./roi_trans_r50_fpn_1x_dota_le90.py']
 
 fp16 = dict(loss_scale='dynamic')
