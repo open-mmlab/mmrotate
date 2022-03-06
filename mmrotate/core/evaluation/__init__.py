4b5535f3dc620445b2bb7c4e685ddd8388743fe2
liuyanyi <wolfsonliu@163.com>
2022-03-06 12:51:00 +0800

    remove recall

--- mmrotate/core/evaluation/__init__.py
+++ mmrotate/core/evaluation/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .eval_map import eval_rbbox_map
-from .recall import eval_recalls
 
-__all__ = ['eval_rbbox_map', 'eval_recalls']
+__all__ = ['eval_rbbox_map']
