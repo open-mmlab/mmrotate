60ca7ca9320a64302cfbbad783db8fe478b4d97b
erenzhou <592267829@qq.com>
2022-03-12 15:09:31 +0800

    fix lint

--- mmrotate/models/dense_heads/rotated_anchor_head.py
+++ mmrotate/models/dense_heads/rotated_anchor_head.py
@@ -29,9 +29,9 @@ class RotatedAnchorHead(BaseDenseHead):
         reg_decoded_bbox (bool): If true, the regression loss would be
             applied on decoded bounding boxes. Default: False
         assign_by_circumhbbox (str): If None, assigner will assign according to
-            the IoU between anchor and GT (OBB), called RetinaNet-OBB.  
+            the IoU between anchor and GT (OBB), called RetinaNet-OBB.
             If angle definition method, assigner will assign according to the
-            IoU between anchor and GT's circumbox (HBB), called RetinaNet-HBB.     
+            IoU between anchor and GT's circumbox (HBB), called RetinaNet-HBB.
         loss_cls (dict): Config of classification loss.
         loss_bbox (dict): Config of localization loss.
         train_cfg (dict): Training config of anchor head.
