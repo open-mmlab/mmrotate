98254e12199ddca3bf5ba3666233e0e4aa5bbca0
yangxue <yangxue0827@126.com>
2022-03-24 18:10:11 +0800

    Add description to anchor generator

--- mmrotate/core/anchor/anchor_generator.py
+++ mmrotate/core/anchor/anchor_generator.py
@@ -3,13 +3,15 @@ import torch
 from mmcv.utils import to_2tuple
 from mmdet.core.anchor import AnchorGenerator
 
-from ..bbox.transforms import hbb2obb
 from .builder import ROTATED_ANCHOR_GENERATORS
 
 
 @ROTATED_ANCHOR_GENERATORS.register_module()
 class RotatedAnchorGenerator(AnchorGenerator):
-    """Fake rotate anchor generator for 2D anchor-based detectors."""
+    """Fake rotate anchor generator for 2D anchor-based detectors.
+
+    Horizontal bounding box represented by (x,y,w,h,theta).
+    """
 
     def single_level_grid_priors(self,
                                  featmap_size,
@@ -35,52 +37,15 @@ class RotatedAnchorGenerator(AnchorGenerator):
         anchors = super(RotatedAnchorGenerator, self).single_level_grid_priors(
             featmap_size, level_idx, dtype=dtype, device=device)
 
+        # The correct usage is：
+        #       from ..bbox.transforms import hbb2obb
+        #       anchors = hbb2obb(anchors, self.angle_version)
+        # However, the experiment shows that the performance has decreased.
         num_anchors = anchors.size(0)
         xy = (anchors[:, 2:] + anchors[:, :2]) / 2
         wh = anchors[:, 2:] - anchors[:, :2]
         theta = xy.new_zeros((num_anchors, 1))
         anchors = torch.cat([xy, wh, theta], axis=1)
-
-        return anchors
-
-
-@ROTATED_ANCHOR_GENERATORS.register_module()
-class FakeRotatedAnchorGenerator(AnchorGenerator):
-    """Fake rotate anchor generator for 2D anchor-based detectors."""
-
-    def __init__(self, angle_version='oc', **kwargs):
-        super(FakeRotatedAnchorGenerator, self).__init__(**kwargs)
-        self.angle_version = angle_version
-
-    def single_level_grid_priors(self,
-                                 featmap_size,
-                                 level_idx,
-                                 dtype=torch.float32,
-                                 device='cuda'):
-        """Generate grid anchors of a single level.
-
-        Note:
-            This function is usually called by method ``self.grid_priors``.
-
-        Args:
-            featmap_size (tuple[int]): Size of the feature maps.
-            level_idx (int): The index of corresponding feature map level.
-            dtype (obj:`torch.dtype`): Date type of points.Defaults to
-            ``torch.float32``.
-            device (str, optional): The device the tensor will be put on.
-            Defaults to 'cuda'.
-
-        Returns:
-            torch.Tensor: Anchors in the overall feature maps.
-        """
-        anchors = super(FakeRotatedAnchorGenerator,
-                        self).single_level_grid_priors(
-                            featmap_size,
-                            level_idx,
-                            dtype=dtype,
-                            device=device)
-
-        anchors = hbb2obb(anchors, self.angle_version)
 
         return anchors
 
