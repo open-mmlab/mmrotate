941d75f3c6c5c5ff0e69d72c952c0eb2c4ecc25d
erenzhou <592267829@qq.com>
2022-04-07 12:25:01 +0800

    Revert "Update __init__.py"
    This reverts commit ba335ea003ce882e06800786a76b40e05a5b9baf.

--- mmrotate/__init__.py
+++ mmrotate/__init__.py
@@ -20,7 +20,7 @@ def digit_version(version_str):
     return digit_version
 
 
-mmcv_minimum_version = '1.4.6'
+mmcv_minimum_version = '1.4.5'
 mmcv_maximum_version = '1.6.0'
 mmcv_version = digit_version(mmcv.__version__)
 
