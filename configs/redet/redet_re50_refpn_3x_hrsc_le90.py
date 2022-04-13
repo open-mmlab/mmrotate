3561f26b9d2814ffdbb74a65ce48e5e89c04952f
yangxue <yangxue0827@126.com>
2022-04-13 11:02:05 +0800

    delete the duplicate part

--- configs/redet/redet_re50_refpn_3x_hrsc_le90.py
+++ configs/redet/redet_re50_refpn_3x_hrsc_le90.py
@@ -173,59 +173,15 @@ model = dict(
             nms=dict(iou_thr=0.1),
             max_per_img=2000)))
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='RResize', img_scale=(800, 512)),
-    dict(type='RRandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(800, 512),
-        flip=False,
-        transforms=[
-            dict(type='RResize'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img'])
-        ])
-]
+test_pipeline = [dict(img_scale=(800, 512))]
 
-dataset_type = 'HRSCDataset'
-data_root = '/data/dataset_share/HRSC2016/HRSC2016/'
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
-    train=dict(
-        type=dataset_type,
-        classwise=False,
-        ann_file=data_root + 'ImageSets/trainval.txt',
-        ann_subdir=data_root + 'FullDataSet/Annotations/',
-        img_subdir=data_root + 'FullDataSet/AllImages/',
-        pipeline=train_pipeline),
-    val=dict(
-        type=dataset_type,
-        classwise=False,
-        ann_file=data_root + 'ImageSets/test.txt',
-        ann_subdir=data_root + 'FullDataSet/Annotations/',
-        img_subdir=data_root + 'FullDataSet/AllImages/',
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        classwise=False,
-        ann_file=data_root + 'ImageSets/test.txt',
-        ann_subdir=data_root + 'FullDataSet/Annotations/',
-        img_subdir=data_root + 'FullDataSet/AllImages/',
-        pipeline=test_pipeline))
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
 
 evaluation = dict(interval=12, metric='mAP')
 optimizer = dict(lr=0.01)
