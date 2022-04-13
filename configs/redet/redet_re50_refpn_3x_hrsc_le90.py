2c2283c8a73a7042d85d0e41c0fe508682d92326
yangxue <yangxue0827@126.com>
2022-04-13 12:13:00 +0800

    Update redet_re50_refpn_3x_hrsc_le90.py

--- configs/redet/redet_re50_refpn_3x_hrsc_le90.py
+++ configs/redet/redet_re50_refpn_3x_hrsc_le90.py
@@ -173,10 +173,32 @@ model = dict(
             nms=dict(iou_thr=0.1),
             max_per_img=2000)))
 
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='RResize', img_scale=(800, 512)),
+    dict(type='RRandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
-test_pipeline = [dict(img_scale=(800, 512))]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(800, 512),
+        flip=False,
+        transforms=[
+            dict(type='RResize'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
 
 data = dict(
     train=dict(pipeline=train_pipeline),
