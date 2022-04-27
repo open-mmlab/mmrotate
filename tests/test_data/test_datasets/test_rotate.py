f1e37b5e5960cbd0b8f5dddf8446507bf84b86be
erenzhou <592267829@qq.com>
2022-04-27 13:31:39 +0800

    Update test_rotate.py

--- tests/test_data/test_datasets/test_rotate.py
+++ tests/test_data/test_datasets/test_rotate.py
@@ -1,7 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
-import pytest
-import torch
 from mmdet.datasets import build_dataset
 
 from mmrotate.datasets.sar import SARDataset
@@ -18,9 +16,6 @@ def _create_dummy_results():
     return [boxes]
 
 
-@pytest.mark.skipif(
-    not torch.cuda.is_available(), reason='requires CUDA support')
-@pytest.mark.parametrize('angle_version', ['oc'])
 def test_dota_dataset(angle_version):
     """Test DOTA dataset.
 
