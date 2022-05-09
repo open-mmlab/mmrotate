585a8400f46722c5aa0f9381e9bf9ada114f30da
remi-or <remi.ouazan--reboul@mines-paristech.fr>
2022-05-08 21:57:00 -0400

    Fixed linting of pipelines __init__

--- mmrotate/datasets/pipelines/__init__.py
+++ mmrotate/datasets/pipelines/__init__.py
@@ -1,7 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .loading import LoadPatchFromImage
-from .transforms import (PolyDiscreteRotate, PolyRandomRotate,
-                         RRandomFlip, RResize)
+from .transforms import (PolyDiscreteRotate, PolyRandomRotate, RRandomFlip,
+                         RResize)
 
-__all__ = ['LoadPatchFromImage', 'RResize', 'RRandomFlip', 'PolyRandomRotate',
-           'PolyDiscreteRotate']
+__all__ = [
+    'LoadPatchFromImage', 'RResize', 'RRandomFlip', 'PolyRandomRotate',
+    'PolyDiscreteRotate'
+]
