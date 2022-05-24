9ba4d9798a17e7e9044b8f98e25542d5d10d9198
nijkah <nijkah@gmail.com>
2022-05-24 19:00:12 +0000

    fix test code

--- tests/test_data/test_pipelines/test_rtransforms.py
+++ tests/test_data/test_pipelines/test_rtransforms.py
@@ -137,18 +137,20 @@ def test_rrandom_crop():
     img = np.zeros([256, 256, 3], dtype=np.uint8)
     results['img'] = img
     results['img_shape'] = img.shape
+    results['gt_bboxes'] = np.array([[50., 40., 2., 1., 0.]], dtype=np.float32)
+    results['gt_bboxes_ignore'] = np.array([[50., 42., 3., 1., 0.]],
+                                           dtype=np.float32)
 
     h, w, c = results['img_shape']
     gt_bboxes = results['gt_bboxes'].copy()
     gt_bboxes_ignore = results['gt_bboxes_ignore'].copy()
 
-    transform = dict(type='RRandomCrop', crop_size=(h - 1, w - 3))
+    transform = dict(type='RRandomCrop', crop_size=(h - 10, w - 30))
     crop_module = build_from_cfg(transform, PIPELINES)
-    print(results['img'].shape)
     results = crop_module(results)
-    assert results['img'].shape[:2] == (h - 1, w - 3)
+    assert results['img'].shape[:2] == (h - 10, w - 30)
     # All bboxes should be reserved after crop
-    assert results['img_shape'][:2] == (h - 1, w - 3)
+    assert results['img_shape'][:2] == (h - 10, w - 30)
     assert results['gt_labels'].shape[0] == results['gt_bboxes'].shape[0]
     assert results['gt_labels'].dtype == np.int64
     assert results['gt_bboxes'].dtype == np.float32
