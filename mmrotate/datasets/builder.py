d1f9595421398d4b6a61c154d970850c1952e9d1
liuyanyi <wolfsonliu@163.com>
2022-06-03 12:39:41 +0800

    Add MultiImageMixDataset in build_dataset

--- mmrotate/datasets/builder.py
+++ mmrotate/datasets/builder.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import copy
 import platform
 
 from mmcv.utils import build_from_cfg
@@ -20,7 +21,9 @@ if platform.system() != 'Windows':
 
 def build_dataset(cfg, default_args=None):
     from mmdet.datasets.dataset_wrappers import (ClassBalancedDataset,
-                                                 ConcatDataset, RepeatDataset)
+                                                 ConcatDataset,
+                                                 MultiImageMixDataset,
+                                                 RepeatDataset)
     if isinstance(cfg, (list, tuple)):
         dataset = ConcatDataset([build_dataset(c, default_args) for c in cfg])
     elif cfg['type'] == 'ConcatDataset':
@@ -33,6 +36,11 @@ def build_dataset(cfg, default_args=None):
     elif cfg['type'] == 'ClassBalancedDataset':
         dataset = ClassBalancedDataset(
             build_dataset(cfg['dataset'], default_args), cfg['oversample_thr'])
+    elif cfg['type'] == 'MultiImageMixDataset':
+        cp_cfg = copy.deepcopy(cfg)
+        cp_cfg['dataset'] = build_dataset(cp_cfg['dataset'])
+        cp_cfg.pop('type')
+        dataset = MultiImageMixDataset(**cp_cfg)
     elif isinstance(cfg.get('ann_file'), (list, tuple)):
         dataset = _concat_dataset(cfg, default_args)
     else:
