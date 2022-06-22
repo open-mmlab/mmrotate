29f79916f9c3afaa3d5edd431461eeab6b75f175
yangxue <yangxue0827@126.com>
2022-06-22 20:32:00 +0800

    [Feature] Support ConvNeXt (CVPR'22) (#343)

---
+++ configs/kld/rotated_retinanet_obb_kld_stable_r50_adamw_fpn_1x_dota_le90.py
@@ -0,0 +1,14 @@
+_base_ = ['./rotated_retinanet_obb_kld_stable_r50_fpn_1x_dota_le90.py']
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
