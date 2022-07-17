4a342b92134b1075f237b30b9d26b6841efd3a8b
liuyanyi <wolfsonliu@163.com>
2022-07-17 17:12:55 +0800

    configs

---
+++ configs/rotated_yolox/rotated_yolox_s_kld_300e_dota.py
@@ -0,0 +1,13 @@
+_base_ = './rotated_yolox_s_300e_dota.py'
+
+# model settings
+model = dict(
+    bbox_head=dict(
+        loss_bbox=dict(
+            _delete_=True,
+            type='GDLoss',
+            loss_type='kld',
+            fun='log1p',
+            tau=1,
+            sqrt=False,
+            loss_weight=27.5)))
