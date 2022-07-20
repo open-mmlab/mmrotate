6844def1986f3848fe01a0f090849855205fdb79
liuyanyi <wolfsonliu@163.com>
2022-07-20 10:15:39 +0800

    add

--- mmrotate/datasets/pipelines/__init__.py
+++ mmrotate/datasets/pipelines/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .loading import FilterRotatedAnnotations, LoadPatchFromImage
-from .transforms import (PolyRandomAffine, PolyRandomRotate, RMosaic,
-                         RRandomFlip, RResize)
+from .transforms import (PolyMixUp, PolyRandomAffine, PolyRandomRotate,
+                         RMosaic, RRandomFlip, RResize)
 
 __all__ = [
     'LoadPatchFromImage', 'RResize', 'RRandomFlip', 'PolyRandomRotate',
-    'RMosaic', 'FilterRotatedAnnotations', 'PolyRandomAffine'
+    'RMosaic', 'FilterRotatedAnnotations', 'PolyRandomAffine', 'PolyMixUp'
 ]
