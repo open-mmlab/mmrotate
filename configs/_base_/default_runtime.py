d89add9fbd688de679a1fec626be6efc4fb854d6
zytx121 <592267829@qq.com>
2022-08-05 12:09:18 +0800

    Update default_runtime.py

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -13,6 +13,12 @@ env_cfg = dict(
     dist_cfg=dict(backend='nccl'),
 )
 
+# TODO: Visualizer is not ready.
+# vis_backends = [dict(type='LocalVisBackend')]
+# visualizer = dict(
+#     type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+
 log_level = 'INFO'
 load_from = None
 resume = False
