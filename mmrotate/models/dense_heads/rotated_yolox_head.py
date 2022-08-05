ed609afd8a7b6b8873b05fcdbdc70d4c3015677e
liuyanyi <wolfsonliu@163.com>
2022-08-05 16:45:45 +0800

    use mmcv.ops.batched_nms

--- mmrotate/models/dense_heads/rotated_yolox_head.py
+++ mmrotate/models/dense_heads/rotated_yolox_head.py
@@ -1,11 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Dict, Optional, Tuple
 
 import numpy as np
 import torch
 import torch.nn as nn
 from mmcv.cnn import bias_init_with_prob
-from mmcv.ops import nms_rotated  # noqa
+from mmcv.ops import batched_nms
 from mmcv.runner import force_fp32
 from mmdet.core import bbox_cxcywh_to_xyxy, multi_apply, reduce_mean
 from mmdet.models.dense_heads import YOLOXHead
@@ -479,113 +478,3 @@ class RotatedYOLOXHead(YOLOXHead):
             return torch.cat([l1_target, angle_target], dim=-1)
         else:
             return l1_target
-
-
-def batched_nms(
-        boxes: torch.Tensor,
-        scores: torch.Tensor,
-        idxs: torch.Tensor,
-        nms_cfg: Optional[Dict],
-        class_agnostic: bool = False) -> Tuple[torch.Tensor, torch.Tensor]:
-    r"""Performs non-maximum suppression in a batched fashion.
-    Modified from `torchvision/ops/boxes.py#L39
-    <https://github.com/pytorch/vision/blob/
-    505cd6957711af790211896d32b40291bea1bc21/torchvision/ops/boxes.py#L39>`_.
-    In order to perform NMS independently per class, we add an offset to all
-    the boxes. The offset is dependent only on the class idx, and is large
-    enough so that boxes from different classes do not overlap.
-
-    Note:
-        Same with mmcv.ops.batched_nms in mmcv 1.5.3.
-    Args:
-        boxes (torch.Tensor): boxes in shape (N, 4) or (N, 5).
-        scores (torch.Tensor): scores in shape (N, ).
-        idxs (torch.Tensor): each index value correspond to a bbox cluster,
-            and NMS will not be applied between elements of different idxs,
-            shape (N, ).
-        nms_cfg (dict | None): Supports skipping the nms when `nms_cfg`
-            is None, otherwise it should specify nms type and other
-            parameters like `iou_thr`. Possible keys includes the following.
-            - iou_thr (float): IoU threshold used for NMS.
-            - split_thr (float): threshold number of boxes. In some cases the
-              number of boxes is large (e.g., 200k). To avoid OOM during
-              training, the users could set `split_thr` to a small value.
-              If the number of boxes is greater than the threshold, it will
-              perform NMS on each group of boxes separately and sequentially.
-              Defaults to 10000.
-        class_agnostic (bool): if true, nms is class agnostic,
-            i.e. IoU thresholding happens over all boxes,
-            regardless of the predicted class. Defaults to False.
-    Returns:
-        tuple: kept dets and indice.
-        - boxes (Tensor): Bboxes with score after nms, has shape
-          (num_bboxes, 5). last dimension 5 arrange as
-          (x1, y1, x2, y2, score)
-        - keep (Tensor): The indices of remaining boxes in input
-          boxes.
-    """
-    # skip nms when nms_cfg is None
-    if nms_cfg is None:
-        scores, inds = scores.sort(descending=True)
-        boxes = boxes[inds]
-        return torch.cat([boxes, scores[:, None]], -1), inds
-
-    nms_cfg_ = nms_cfg.copy()
-    class_agnostic = nms_cfg_.pop('class_agnostic', class_agnostic)
-    if class_agnostic:
-        boxes_for_nms = boxes
-    else:
-        # When using rotated boxes, only apply offsets on center.
-        if boxes.size(-1) == 5:
-            max_coordinate = boxes[..., :2].max()
-            max_size = boxes[..., 2:4].max()
-            max_coordinate = max_coordinate + max_size
-            offsets = idxs.to(boxes) * (
-                max_coordinate + torch.tensor(1).to(boxes))
-            boxes_ctr_for_nms = boxes[..., :2] + offsets[:, None]
-            boxes_for_nms = torch.cat([boxes_ctr_for_nms, boxes[..., 2:5]],
-                                      dim=-1)
-        else:
-            max_coordinate = boxes.max()
-            offsets = idxs.to(boxes) * (
-                max_coordinate + torch.tensor(1).to(boxes))
-            boxes_for_nms = boxes + offsets[:, None]
-
-    nms_type = nms_cfg_.pop('type', 'nms_rotated')
-    nms_op = eval(nms_type)
-
-    split_thr = nms_cfg_.pop('split_thr', 10000)
-    # Won't split to multiple nms nodes when exporting to onnx
-    if boxes_for_nms.shape[0] < split_thr or torch.onnx.is_in_onnx_export():
-        dets, keep = nms_op(boxes_for_nms, scores, **nms_cfg_)
-        boxes = boxes[keep]
-
-        # This assumes `dets` has arbitrary dimensions where
-        # the last dimension is score.
-        # Currently it supports bounding boxes [x1, y1, x2, y2, score] or
-        # rotated boxes [cx, cy, w, h, angle_radian, score].
-
-        scores = dets[:, -1]
-    else:
-        max_num = nms_cfg_.pop('max_num', -1)
-        total_mask = scores.new_zeros(scores.size(), dtype=torch.bool)
-        # Some type of nms would reweight the score, such as SoftNMS
-        scores_after_nms = scores.new_zeros(scores.size())
-        for id in torch.unique(idxs):
-            mask = (idxs == id).nonzero(as_tuple=False).view(-1)
-            dets, keep = nms_op(boxes_for_nms[mask], scores[mask], **nms_cfg_)
-            total_mask[mask[keep]] = True
-            scores_after_nms[mask[keep]] = dets[:, -1]
-        keep = total_mask.nonzero(as_tuple=False).view(-1)
-
-        scores, inds = scores_after_nms[keep].sort(descending=True)
-        keep = keep[inds]
-        boxes = boxes[keep]
-
-        if max_num > 0:
-            keep = keep[:max_num]
-            boxes = boxes[:max_num]
-            scores = scores[:max_num]
-
-    boxes = torch.cat([boxes, scores[:, None]], -1)
-    return boxes, keep
