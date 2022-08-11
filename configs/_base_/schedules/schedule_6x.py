45224a3099747ae0b2278b1ffdafa426ce312cdc
Yue Zhou <592267829@qq.com>
2022-08-11 10:26:59 +0800

    [Refactor] default runtime config (#447)
    * init
    
    * Update default_runtime.py

--- configs/_base_/schedules/schedule_6x.py
+++ configs/_base_/schedules/schedule_6x.py
@@ -1,14 +1,27 @@
-# evaluation
-evaluation = dict(interval=1, metric='mAP')
+# training schedule for 1x
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=72, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0 / 3,
+        by_epoch=False,
+        begin=0,
+        end=500),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=72,
+        by_epoch=True,
+        milestones=[48, 66],
+        gamma=0.1)
+]
+
 # optimizer
-optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=1.0 / 3,
-    step=[48, 66])
-runner = dict(type='EpochBasedRunner', max_epochs=72)
-checkpoint_config = dict(interval=1)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001),
+    clip_grad=dict(max_norm=35, norm_type=2))
