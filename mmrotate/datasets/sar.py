a3c7b0df048dcd732e94c982a7dd14273f9caccb
zytx121 <592267829@qq.com>
2022-08-12 12:07:23 +0800

    init

--- mmrotate/datasets/sar.py
+++ mmrotate/datasets/sar.py
@@ -1,12 +1,111 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .builder import ROTATED_DATASETS
-from .dota import DOTADataset
+import glob
+import os.path as osp
+from typing import List
+
+from mmengine.dataset import BaseDataset
+
+from mmrotate.registry import DATASETS
 
 
-@ROTATED_DATASETS.register_module()
-class SARDataset(DOTADataset):
-    """SAR ship dataset for detection (Support RSSDD and HRSID)."""
-    CLASSES = ('ship', )
-    PALETTE = [
-        (0, 255, 0),
-    ]
+@DATASETS.register_module()
+class SARDataset(BaseDataset):
+    """SAR ship dataset for detection (Support SSDD and HRSID).
+
+    Note: ``SARDataset`` need annotations to provide image shape information.
+    If the original annotation has no shape information, you must use the tool
+    provided by us to add image shape at the end line of the annotation. The
+    tool can be found at: ``tools/data/add_img_shape.py``.
+
+    Args:
+        diff_thr (int): The difficulty threshold of ground truth. Bboxes
+            with difficulty higher than it will be ignored. The range of this
+            value should be non-negative integer. Defaults to 100.
+    """
+    METAINFO = {
+        'CLASSES': ('ship', ),
+        # PALETTE is a list of color tuples, which is used for visualization.
+        'PALETTE': [
+            (0, 255, 0),
+        ]
+    }
+
+    def __init__(self, diff_thr: int = 100, **kwargs) -> None:
+        self.diff_thr = diff_thr
+        super().__init__(**kwargs)
+
+    def load_data_list(self) -> List[dict]:
+        """Load annotations from an annotation file named as ``self.ann_file``
+        Returns:
+            List[dict]: A list of annotation.
+        """  # noqa: E501
+        cls_map = {c: i
+                   for i, c in enumerate(self.metainfo['CLASSES'])
+                   }  # in mmdet v2.0 label is 0-based
+        data_list = []
+        txt_files = glob.glob(osp.join(self.ann_file, '*.txt'))
+        if len(txt_files) == 0:
+            raise ValueError('There is no txt file in ' f'{self.ann_file}')
+        for txt_file in txt_files:
+            data_info = {}
+            img_id = osp.split(txt_file)[1][:-4]
+            data_info['img_id'] = img_id
+            img_name = img_id + '.png'
+            data_info['file_name'] = img_name
+            data_info['img_path'] = osp.join(self.data_prefix['img_path'],
+                                             img_name)
+
+            instances = []
+            with open(txt_file) as f:
+                s = f.readlines()
+                for si in s[:-1]:
+                    instance = {}
+                    bbox_info = si.split()
+                    instance['bbox'] = [float(i) for i in bbox_info[:8]]
+                    cls_name = bbox_info[8]
+                    instance['bbox_label'] = cls_map[cls_name]
+                    difficulty = int(bbox_info[9])
+                    if difficulty > self.diff_thr:
+                        instance['ignore_flag'] = 1
+                    else:
+                        instance['ignore_flag'] = 0
+                    instances.append(instance)
+                width, height = s[-1].split()
+                data_info['width'] = int(width)
+                data_info['height'] = int(height)
+            data_info['instances'] = instances
+            data_list.append(data_info)
+
+        return data_list
+
+    def filter_data(self) -> List[dict]:
+        """Filter annotations according to filter_cfg.
+
+        Returns:
+            List[dict]: Filtered results.
+        """
+        if self.test_mode:
+            return self.data_list
+
+        filter_empty_gt = self.filter_cfg.get('filter_empty_gt', False) \
+            if self.filter_cfg is not None else False
+
+        valid_data_infos = []
+        for i, data_info in enumerate(self.data_list):
+            if filter_empty_gt and len(data_info['instances']) == 0:
+                continue
+            valid_data_infos.append(data_info)
+
+        return valid_data_infos
+
+    def get_cat_ids(self, idx: int) -> List[int]:
+        """Get category ids by index.
+
+        Args:
+            idx (int): Index of data.
+        Returns:
+            List[int]: All categories in the image of specified index.
+        """
+
+        instances = self.get_data_info(idx)['instances']
+        return [instance['bbox_label'] for instance in instances]
---
+++ tools/data/add_img_shape.py
@@ -0,0 +1,100 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import argparse
+import os
+import os.path as osp
+import time
+from functools import partial
+from multiprocessing import Pool
+import shutil
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
+
+
+def add_parser(parser):
+    """Add arguments."""
+    parser.add_argument(
+        '--nproc', type=int, default=10, help='the procession number')
+
+    # argument for loading data
+    parser.add_argument(
+        '--data-dir',
+        type=str,
+        default=None,
+        help='images and annotations dirs, must give a value')
+
+
+def parse_args():
+    """Parse arguments."""
+    parser = argparse.ArgumentParser(description='Add image shape in the end line of annotations')
+    add_parser(parser)
+    args = parser.parse_args()
+    assert args.data_dir is not None, "argument img_dir can't be None"
+    return args
+
+
+def load_data(img_dir, ann_dir, save_dir, nproc=10):
+    """Load dataset.
+
+    Args:
+        img_dir (str): Path of images.
+        ann_dir (str): Path of annotations.
+        save_dir (str): Path of new annotations.
+        nproc (int): number of processes.
+
+    Returns:
+        list: Dataset's contents.
+    """
+    assert osp.isdir(img_dir), f'The {img_dir} is not an existing dir!'
+    assert osp.isdir(ann_dir), f'The {ann_dir} is not an existing dir!'
+
+    _load_func = partial(_load_data_single, img_dir=img_dir, ann_dir=ann_dir, save_dir=save_dir)
+    if nproc > 1:
+        pool = Pool(nproc)
+        pool.map(_load_func, os.listdir(img_dir))
+        pool.close()
+    else:
+        list(map(_load_func, os.listdir(img_dir)))
+
+
+def _load_data_single(imgfile, img_dir, ann_dir, save_dir):
+    """Load single image.
+
+    Args:
+        imgfile (str): Filename of single image.
+        img_dir (str): Path of images.
+        ann_dir (str): Path of annotations.
+        save_dir (str): Path of new annotations.
+
+    Returns:
+        dict: Content of single image.
+    """
+    img_id, ext = osp.splitext(imgfile)
+    imgpath = osp.join(img_dir, imgfile)
+    size = Image.open(imgpath).size
+    txtfile = osp.join(ann_dir, img_id + '.txt')
+    dstfile = osp.join(save_dir, img_id + '.txt')
+    shutil.copyfile(txtfile, dstfile)
+    with open(dstfile, 'a') as f:
+        f.write(f'{size[0]} {size[1]}\n')
+
+
+def main():
+    """Main function of image split."""
+    args = parse_args()
+    img_dir = osp.join(args.data_dir, 'images')
+    ann_dir = osp.join(args.data_dir, 'labelTxt')
+    save_dir = osp.join(args.data_dir, 'annfiles')
+    assert not osp.exists(save_dir), \
+        f'{osp.join(save_dir)} already exists'
+    os.makedirs(save_dir)
+
+    print('Processing...')
+    start = time.time()
+    load_data(img_dir=img_dir, ann_dir=ann_dir, save_dir=save_dir, nproc=args.nproc)
+    stop = time.time()
+    print(f'Finish in {int(stop - start)} second!!!')
+
+
+if __name__ == '__main__':
+    main()
