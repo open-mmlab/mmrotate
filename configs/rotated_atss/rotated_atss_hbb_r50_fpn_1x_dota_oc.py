3505f4336dfddda00e9735edc4141281c4cd3ac9
zytx121 <592267829@qq.com>
2022-08-26 17:45:09 +0800

    init

--- configs/rotated_atss/rotated_atss_hbb_r50_fpn_1x_dota_oc.py
+++ configs/rotated_atss/rotated_atss_hbb_r50_fpn_1x_dota_oc.py
@@ -1,21 +1,27 @@
-_base_ = ['../rotated_retinanet/rotated_retinanet_hbb_r50_fpn_1x_dota_oc.py']
+_base_ = 'rotated_atss_obb_r50_fpn_1x_dota_le90.py'
 
 angle_version = 'oc'
+
 model = dict(
     bbox_head=dict(
-        type='RotatedATSSHead',
-        assign_by_circumhbbox=angle_version,
-        anchor_generator=dict(
-            type='RotatedAnchorGenerator',
-            octave_base_scale=4,
-            scales_per_octave=1,
-            ratios=[1.0],
-            strides=[8, 16, 32, 64, 128]),
-    ),
-    train_cfg=dict(
-        assigner=dict(
-            _delete_=True,
-            type='ATSSObbAssigner',
-            topk=9,
+        anchor_generator=dict(angle_version=angle_version),
+        bbox_coder=dict(
             angle_version=angle_version,
-            iou_calculator=dict(type='RBboxOverlaps2D'))))
+            norm_factor=1,
+            edge_swap=False,
+            proj_xy=True)),
+    train_cfg=dict(
+        assigner=dict(iou_calculator=dict(type='FakeRBboxOverlaps2D'))))
+
+train_pipeline = [
+    dict(
+        type='mmdet.LoadImageFromFile',
+        file_client_args={{_base_.file_client_args}}),
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(type='mmdet.Resize', scale=(1024, 2014), keep_ratio=True),
+    dict(type='mmdet.RandomFlip', prob=0.5),
+    dict(type='mmdet.PackDetInputs')
+]
+
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
