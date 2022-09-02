c95ac9c8be210b7811a1315b86cb36688bc3a62d
zytx121 <592267829@qq.com>
2022-09-03 00:31:25 +0800

    fix lint

--- mmrotate/testing/__init__.py
+++ mmrotate/testing/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from ._utils import get_detector_cfg
+from ._utils import demo_mm_inputs, get_detector_cfg
 
-__all__ = ['get_detector_cfg']
+__all__ = ['get_detector_cfg', 'demo_mm_inputs']
