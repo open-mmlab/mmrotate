ed55342df634f24be8bf428b1315e31ebfd3481a
zytx121 <592267829@qq.com>
2022-09-06 17:03:05 +0800

    init

--- configs/rotated_fcos/rotated_fcos_sep_angle_r50_fpn_1x_dota_le90.py
+++ configs/rotated_fcos/rotated_fcos_sep_angle_r50_fpn_1x_dota_le90.py
@@ -1,14 +1,21 @@
 _base_ = [
-    '../_base_/datasets/dotav1.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/datasets/dota.py', '../_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py'
 ]
 angle_version = 'le90'
 
 # model settings
 model = dict(
-    type='RotatedFCOS',
+    type='mmdet.FCOS',
+    data_preprocessor=dict(
+        type='mmdet.DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=32,
+        boxlist2tensor=False),
     backbone=dict(
-        type='ResNet',
+        type='mmdet.ResNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
@@ -19,7 +26,7 @@ model = dict(
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     neck=dict(
-        type='FPN',
+        type='mmdet.FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
         start_level=1,
@@ -39,45 +46,22 @@ model = dict(
         centerness_on_reg=True,
         separate_angle=True,
         scale_angle=True,
-        bbox_coder=dict(
-            type='DistanceAnglePointCoder', angle_version=angle_version),
-        h_bbox_coder=dict(type='DistancePointBBoxCoder'),
+        h_bbox_coder=dict(type='mmdet.DistancePointBBoxCoder'),
         loss_cls=dict(
-            type='FocalLoss',
+            type='mmdet.FocalLoss',
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
             loss_weight=1.0),
-        loss_bbox=dict(type='GIoULoss', loss_weight=1.0),
-        loss_angle=dict(type='L1Loss', loss_weight=0.2),
+        loss_bbox=dict(type='mmdet.GIoULoss', loss_weight=1.0),
+        loss_angle=dict(type='mmdet.L1Loss', loss_weight=0.2),
         loss_centerness=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
+            type='mmdet.CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
     # training and testing settings
     train_cfg=None,
     test_cfg=dict(
         nms_pre=2000,
         min_bbox_size=0,
         score_thr=0.05,
-        nms=dict(iou_thr=0.1),
+        nms=dict(type='nms_rotated', iou_threshold=0.1),
         max_per_img=2000))
-
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RResize', img_scale=(1024, 1024)),
-    dict(
-        type='RRandomFlip',
-        flip_ratio=[0.25, 0.25, 0.25],
-        direction=['horizontal', 'vertical', 'diagonal'],
-        version=angle_version),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-]
-data = dict(
-    train=dict(pipeline=train_pipeline, version=angle_version),
-    val=dict(version=angle_version),
-    test=dict(version=angle_version))
