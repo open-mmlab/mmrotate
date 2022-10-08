7b43250c14a638e36c801db1065ef5d21dd8544d
zytx121 <592267829@qq.com>
2022-10-08 14:25:49 +0800

    Update test_angle_coder.py

--- tests/test_models/test_task_modules/test_coder/test_angle_coder.py
+++ tests/test_models/test_task_modules/test_coder/test_angle_coder.py
@@ -19,8 +19,7 @@ class TestCSLCoder(TestCase):
 
     def test_decode(self):
         coder = CSLCoder(angle_version='oc', omega=10)
-        encode_angles = torch.Tensor(
-            [[0., 0., 0., 0., 0., 0., 0., 0., 0.]])
+        encode_angles = torch.Tensor([[0., 0., 0., 0., 0., 0., 0., 0., 0.]])
         expected_decode_angles = torch.Tensor([[0.08726]])
         out = coder.decode(encode_angles)
         assert_allclose(expected_decode_angles, out)
