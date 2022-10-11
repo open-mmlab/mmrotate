508350847a4e2e61a83bf0fabf3b7f858786daca
Yanyi Liu <wolfsonliu@163.com>
2022-10-11 10:52:44 +0800

    remove useless class

--- mmrotate/models/detectors/__init__.py
+++ mmrotate/models/detectors/__init__.py
@@ -6,7 +6,6 @@ from .redet import ReDet
 from .refine_single_stage import RefineSingleStageDetector
 from .roi_transformer import RoITransformer
 from .rotate_faster_rcnn import RotatedFasterRCNN
-from .rotated_fcos import RotatedFCOS
 from .rotated_reppoints import RotatedRepPoints
 from .rotated_retinanet import RotatedRetinaNet
 from .single_stage import RotatedSingleStageDetector
@@ -15,6 +14,6 @@ from .two_stage import RotatedTwoStageDetector
 __all__ = [
     'RotatedRetinaNet', 'RotatedFasterRCNN', 'OrientedRCNN', 'RoITransformer',
     'GlidingVertex', 'ReDet', 'RotatedRepPoints', 'RotatedBaseDetector',
-    'RotatedTwoStageDetector', 'RotatedSingleStageDetector', 'RotatedFCOS',
+    'RotatedTwoStageDetector', 'RotatedSingleStageDetector',
     'RefineSingleStageDetector'
 ]
