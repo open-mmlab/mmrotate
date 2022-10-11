2c2fcd04668cfa6f46160941ef9d8dd93ae957c2
zytx121 <592267829@qq.com>
2022-10-11 16:24:22 +0800

    update

--- configs/kfiou/rotated_retinanet_hbb_kfiou_r50_fpn_1x_dota_le135.py
+++ configs/kfiou/rotated_retinanet_hbb_kfiou_r50_fpn_1x_dota_le135.py
@@ -1,7 +1,9 @@
-_base_ = '../rotated_retinanet/rotated_retinanet_hbb_r50_fpn_1x_dota_le135.py'
+_base_ = '../rotated_retinanet/rotated_retinanet_obb_r50_fpn_1x_dota_le135.py'
 
 model = dict(
     bbox_head=dict(
         type='RotatedRetinaHead',
         loss_bbox_type='kfiou',
-        loss_bbox=dict(type='KFLoss', loss_weight=5.0)))
+        loss_bbox=dict(type='KFLoss', loss_weight=5.0)),
+    train_cfg=dict(
+        assigner=dict(iou_calculator=dict(type='FakeRBboxOverlaps2D'))))
