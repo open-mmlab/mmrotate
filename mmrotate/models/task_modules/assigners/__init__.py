998386f91a6579131edd81eb5d7c87cbf1eb9671
zytx121 <592267829@qq.com>
2022-10-21 09:07:09 +0800

    Update __init__.py

--- mmrotate/models/task_modules/assigners/__init__.py
+++ mmrotate/models/task_modules/assigners/__init__.py
@@ -1,12 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from ..assigners.rotate_iou2d_calculator import (FakeRBboxOverlaps2D,
-                                                 QBbox2HBboxOverlaps2D,
-                                                 RBbox2HBboxOverlaps2D,
-                                                 RBboxOverlaps2D,
-                                                 rbbox_overlaps)
 from .atss_kld_assigner import ATSSKldAssigner
 from .convex_assigner import ConvexAssigner
 from .max_convex_iou_assigner import MaxConvexIoUAssigner
+from .rotate_iou2d_calculator import (FakeRBboxOverlaps2D,
+                                      QBbox2HBboxOverlaps2D,
+                                      RBbox2HBboxOverlaps2D, RBboxOverlaps2D,
+                                      rbbox_overlaps)
 from .rotated_atss_assigner import RotatedATSSAssigner
 from .sas_assigner import SASAssigner
 
