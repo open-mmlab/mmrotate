efd481e51efaa10300448843f6452e0594187be1
erenzhou <592267829@qq.com>
2022-10-23 16:52:35 +0800

    Update gliding-vertex-rbox_r50_fpn_1x_dota.py

--- configs/gliding_vertex/gliding-vertex-rbox_r50_fpn_1x_dota.py
+++ configs/gliding_vertex/gliding-vertex-rbox_r50_fpn_1x_dota.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/datasets/dota.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/datasets/dota_qbox.py', '../_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py'
 ]
 
