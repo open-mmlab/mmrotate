d2a4f954a61bbfe70a172d9a4d8f2ededfc10bc6
erenzhou <592267829@qq.com>
2022-10-23 17:39:54 +0800

    update ms configs

--- configs/roi_trans/roi-trans-le90_r50_fpn_1x_dota-ms.py
+++ configs/roi_trans/roi-trans-le90_r50_fpn_1x_dota-ms.py
@@ -1,6 +1,15 @@
-_base_ = './roi-trans-le90_r50_fpn_1x_dota.py'
+_base_ = './roi-trans-le90_r50_fpn_rr-1x_dota-ms.py'
 
-data_root = 'data/split_ms_dota/'
+train_pipeline = [
+    dict(type='mmdet.LoadImageFromFile', file_client_args={{_base_.file_client_args}}),
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(type='mmdet.Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(
+        type='mmdet.RandomFlip',
+        prob=0.75,
+        direction=['horizontal', 'vertical', 'diagonal']),
+    dict(type='mmdet.PackDetInputs')
+]
 
-train_dataloader = dict(dataset=dict(data_root=data_root))
-val_dataloader = dict(dataset=dict(data_root=data_root))
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
