9c1bb33f968ef54777ce3948e64cd9903536a92b
Yanyi Liu <wolfsonliu@163.com>
2022-10-25 10:26:57 +0800

    [Feature] Add RotatedCocoMetric (#557)
    * Add Rotated COCO Metric
    
    * Fix
    
    Co-authored-by: Yue Zhou <592267829@qq.com>

--- mmrotate/evaluation/metrics/__init__.py
+++ mmrotate/evaluation/metrics/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .dota_metric import DOTAMetric
+from .rotated_coco_metric import RotatedCocoMetric
 
-__all__ = ['DOTAMetric']
+__all__ = ['DOTAMetric', 'RotatedCocoMetric']
