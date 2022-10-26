82f5b25c4832226f0fd661bf4094de3819c2160d
jbwang1997 <jbwang1997@gmail.com>
2022-10-26 17:03:53 +0800

    mv patch into mmrotate.utils

--- mmrotate/apis/__init__.py
+++ mmrotate/apis/__init__.py
@@ -1,8 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import inference_detector_by_patches
-from .patch import get_multiscale_patch, merge_results_by_nms, slide_window
 
-__all__ = [
-    'inference_detector_by_patches', 'get_multiscale_patch', 'slide_window',
-    'merge_results_by_nms'
-]
+__all__ = ['inference_detector_by_patches']
--- mmrotate/utils/__init__.py
+++ mmrotate/utils/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .collect_env import collect_env
 from .misc import find_latest_checkpoint, get_test_pipeline_cfg
+from .patch import get_multiscale_patch, merge_results_by_nms, slide_window
 from .setup_env import register_all_modules, setup_multi_processes
 
 __all__ = [
     'collect_env', 'find_latest_checkpoint', 'setup_multi_processes',
-    'register_all_modules', 'get_test_pipeline_cfg'
+    'register_all_modules', 'get_test_pipeline_cfg', 'get_multiscale_patch',
+    'merge_results_by_nms', 'slide_window'
 ]
