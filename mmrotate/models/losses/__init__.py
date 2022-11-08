cacfc88ff316849b4c882b2c2cbd107dc59e5d02
liuyanyi <wolfsonliu@163.com>
2022-11-08 19:04:43 +0800

    clean

--- mmrotate/models/losses/__init__.py
+++ mmrotate/models/losses/__init__.py
@@ -3,11 +3,12 @@ from .convex_giou_loss import BCConvexGIoULoss, ConvexGIoULoss
 from .gaussian_dist_loss import GDLoss
 from .gaussian_dist_loss_v1 import GDLoss_v1
 from .kf_iou_loss import KFLoss
+from .rd_iou_loss import RDIoULoss
 from .rotated_iou_loss import RotatedIoULoss
 from .smooth_focal_loss import SmoothFocalLoss
 from .spatial_border_loss import SpatialBorderLoss
 
 __all__ = [
     'GDLoss', 'GDLoss_v1', 'KFLoss', 'ConvexGIoULoss', 'BCConvexGIoULoss',
-    'SmoothFocalLoss', 'RotatedIoULoss', 'SpatialBorderLoss'
+    'SmoothFocalLoss', 'RotatedIoULoss', 'SpatialBorderLoss', 'RDIoULoss'
 ]
