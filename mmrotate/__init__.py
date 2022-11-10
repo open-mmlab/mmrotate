bc1ced4cafdc8d62305958512b0195d822ff6c7f
Yue Zhou <592267829@qq.com>
2022-11-10 10:21:05 +0800

    Bump mmcv's max version (#595)
    * update
    
    * Update faq.md

--- mmrotate/__init__.py
+++ mmrotate/__init__.py
@@ -21,7 +21,7 @@ def digit_version(version_str):
 
 
 mmcv_minimum_version = '1.5.3'
-mmcv_maximum_version = '1.7.0'
+mmcv_maximum_version = '1.8.0'
 mmcv_version = digit_version(mmcv.__version__)
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
