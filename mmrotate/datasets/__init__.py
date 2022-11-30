e8c6eb8ab44b4d59c0d0fd3a6d65e953fdf384bc
yxzhao <33384732+yxzhao2022@users.noreply.github.com>
2022-11-30 13:54:58 +0800

    [Feature] Support DIOR Dataset (#639)
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * [Feature] Support DIOR Dataset
    
    * Update test_dior.py
    
    * Update dior.py
    
    * Update README.md
    
    * Update metafile.yml

--- mmrotate/datasets/__init__.py
+++ mmrotate/datasets/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .dior import DIORDataset  # noqa: F401, F403
 from .dota import DOTADataset  # noqa: F401, F403
 from .hrsc import HRSCDataset  # noqa: F401, F403
 from .transforms import *  # noqa: F401, F403
 
-__all__ = ['DOTADataset', 'HRSCDataset']
+__all__ = ['DOTADataset', 'HRSCDataset', 'DIORDataset']
