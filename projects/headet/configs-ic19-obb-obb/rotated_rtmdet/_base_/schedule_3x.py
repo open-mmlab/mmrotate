b511063b135b7e21c88e6f6096d91125ffe8283c
vansin <msnode@163.com>
2023-02-05 21:54:52 +0800

    feat: init rtmdet

---
+++ projects/headet/configs-ic19-obb-obb/rotated_rtmdet/_base_/schedule_3x.py
@@ -0,0 +1,33 @@
+max_epochs = 3 * 12
+base_lr = 0.004 / 16
+interval = 12
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=interval)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0e-5,
+        by_epoch=False,
+        begin=0,
+        end=1000),
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=base_lr * 0.05,
+        begin=max_epochs // 2,
+        end=max_epochs,
+        T_max=max_epochs // 2,
+        by_epoch=True,
+        convert_to_iter_based=True),
+]
+
+# optimizer
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=base_lr, weight_decay=0.05),
+    paramwise_cfg=dict(
+        norm_decay_mult=0, bias_decay_mult=0, bypass_duplicate=True))
---
+++ projects/headet/configs-ic19-obb-obb/rotated_rtmdet/rotated_rtmdet_l-3x-dota_pt.py
@@ -0,0 +1,3 @@
+_base_ = './rotated_rtmdet_l-3x-dota.py'
+
+load_from = 'https://download.openmmlab.com/mmrotate/v1.0/rotated_rtmdet/rotated_rtmdet_l-3x-dota/rotated_rtmdet_l-3x-dota-23992372.pth'
