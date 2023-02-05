476f49ffca0ee32a239fb8d51fbe316facf95de6
vansin <msnode@163.com>
2023-02-06 02:19:20 +0800

    feat: update angle h180 in rtmdet

--- projects/headet/configs-ic19-obb-obb/rotated_rtmdet/rotated_rtmdet_l-3x-dota.py
+++ projects/headet/configs-ic19-obb-obb/rotated_rtmdet/rotated_rtmdet_l-3x-dota.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 checkpoint = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_rsb_pretrain/cspnext-l_8xb256-rsb-a1-600e_in1k-6a760974.pth'  # noqa
 
-angle_version = 'le90'
+angle_version = 'h180'
 model = dict(
     type='mmdet.RTMDet',
     data_preprocessor=dict(
