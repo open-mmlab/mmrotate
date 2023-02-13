6c3186500d1d6aa1dc619e89c020d20791244dbf
vansin <msnode@163.com>
2023-02-13 21:42:49 +0800

    feat: add a1l1 loss

--- projects/headet/models/losses/a_l1_loss.py
+++ projects/headet/models/losses/a_l1_loss.py
@@ -24,7 +24,7 @@ def al1_loss(pred: Tensor, target: Tensor) -> Tensor:
 
     assert pred.size() == target.size()
 
-    pred = (pred + np.pi) % (2*np.pi) - np.pi 
+    pred = (pred + np.pi) % (2*np.pi) - np.pi
     assert pred.max() < np.pi
     assert pred.min() > -np.pi
     assert target.max() < np.pi
@@ -34,7 +34,6 @@ def al1_loss(pred: Tensor, target: Tensor) -> Tensor:
                              np.pi-loss, loss)
 
     return loss
-
 
 @MODELS.register_module()
 class AL1Loss(nn.Module):
@@ -79,5 +78,82 @@ class AL1Loss(nn.Module):
         reduction = (
             reduction_override if reduction_override else self.reduction)
         loss_bbox = self.loss_weight * al1_loss(
+            pred, target, weight, reduction=reduction, avg_factor=avg_factor)
+        return loss_bbox
+
+@weighted_loss
+def a1l1_loss(pred: Tensor, target: Tensor) -> Tensor:
+    """AL1 loss.
+
+    Args:
+        pred (Tensor): The prediction.
+        target (Tensor): The learning target of the prediction.
+
+    Returns:
+        Tensor: Calculated loss
+    """
+    if target.numel() == 0:
+        return pred.sum() * 0
+
+    loss_pred = torch.where((pred >= -np.pi) & (pred<np.pi), torch.abs(pred*0), torch.abs(pred))
+
+
+    assert pred.size() == target.size()
+
+    pred = (pred + np.pi) % (2*np.pi) - np.pi
+
+    assert pred.max() < np.pi
+    assert pred.min() > -np.pi
+    assert target.max() < np.pi
+    assert target.min() > -np.pi
+    loss = torch.abs(pred-target)
+    loss = torch.where(loss > np.pi, 2 *
+                             np.pi-loss, loss)
+
+    return loss+loss_pred
+
+@MODELS.register_module()
+class A1L1Loss(nn.Module):
+    """L1 loss.
+
+    Args:
+        reduction (str, optional): The method to reduce the loss.
+            Options are "none", "mean" and "sum".
+        loss_weight (float, optional): The weight of loss.
+    """
+
+    def __init__(self,
+                 reduction: str = 'mean',
+                 loss_weight: float = 1.0) -> None:
+        super().__init__()
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def forward(self,
+                pred: Tensor,
+                target: Tensor,
+                weight: Optional[Tensor] = None,
+                avg_factor: Optional[int] = None,
+                reduction_override: Optional[str] = None) -> Tensor:
+        """Forward function.
+
+        Args:
+            pred (Tensor): The prediction.
+            target (Tensor): The learning target of the prediction.
+            weight (Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Defaults to None.
+
+        Returns:
+            Tensor: Calculated loss
+        """
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        loss_bbox = self.loss_weight * a1l1_loss(
             pred, target, weight, reduction=reduction, avg_factor=avg_factor)
         return loss_bbox
