14d6294e5e0bac0266bb3912a63f31616a8a9df7
vansin <msnode@163.com>
2023-02-15 21:48:21 +0800

    feat: delete the unuse

--- projects/RR360/configs360/roi_trans_3x/roi-trans-le90_swin-tiny_fpn_3x_dota_pt.py
+++
@@ -1,3 +0,0 @@
-_base_ = './roi-trans-le90_swin-tiny_fpn_3x_dota.py'
-
-load_from = 'https://download.openmmlab.com/mmrotate/v0.1.0/roi_trans/roi_trans_swin_tiny_fpn_1x_dota_le90/roi_trans_swin_tiny_fpn_1x_dota_le90-ddeee9ae.pth'
