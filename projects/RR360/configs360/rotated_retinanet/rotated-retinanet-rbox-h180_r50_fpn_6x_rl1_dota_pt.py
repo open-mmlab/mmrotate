99786dc66c2344dea4c8eefd69ed28e9bae51dd1
vansin <msnode@163.com>
2023-02-15 22:13:31 +0800

    delete unuse

--- projects/RR360/configs360/rotated_retinanet/rotated-retinanet-rbox-h180_r50_fpn_6x_rl1_dota_pt.py
+++
@@ -1,3 +0,0 @@
-_base_ = 'rotated-retinanet-rbox-h180_r50_fpn_6x_rl1_dota.py'
-
-load_from = 'https://download.openmmlab.com/mmrotate/v0.1.0/rotated_retinanet/rotated_retinanet_obb_r50_fpn_1x_dota_le135/rotated_retinanet_obb_r50_fpn_1x_dota_le135-e4131166.pth'
