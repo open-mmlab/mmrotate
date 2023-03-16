d88a602e6cd59980d69d1c210aa78ec203edeba0
zytx121 <592267829@qq.com>
2023-03-16 17:56:51 +0800

    update

--- configs/rotated_rtmdet/rotated_rtmdet_l-3x-dota.py
+++ configs/rotated_rtmdet/rotated_rtmdet_l-3x-dota.py
@@ -1,6 +1,6 @@
 _base_ = [
     './_base_/default_runtime.py', './_base_/schedule_3x.py',
-    './_base_/dota_rr.py'
+    './_base_/dota_rr.py', './_base_/tta.py'
 ]
 checkpoint = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_rsb_pretrain/cspnext-l_8xb256-rsb-a1-600e_in1k-6a760974.pth'  # noqa
 
