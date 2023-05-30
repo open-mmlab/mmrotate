f011cda5593add698e84838f13185a0d9f63d9b8
zytx121 <592267829@qq.com>
2023-05-30 20:09:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,10 +85,7 @@ with gr.Blocks() as demo:
         with gr.Row():
             # paths, _ = get_file_list('demo')
             example_images = gr.Dataset(
-                components=[input_image], samples=[
-                os.path.join("examples", e)
-                for e in os.listdir("examples")
-            ],)
+                components=[input_image], samples=["examples/demo.jpg", ],)
 
         run_button.click(
             fn=inference,
