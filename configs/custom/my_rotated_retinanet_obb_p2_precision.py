938641092d969c43ae7bf78c45e6ba33baeb8d1e
Sara Bała <sara@illumicell.ai>
2025-10-03 12:19:41 +0200

    added data and new config file

---
+++ configs/custom/my_rotated_retinanet_obb_p2_precision.py
@@ -0,0 +1,228 @@
+from mmrotate.models import *
+
+
+angle_version = 'le90'
+
+# -----------------------------
+# Model (P2 enabled; 320x320)
+# -----------------------------
+model = dict(
+    type='RotatedRetinaNet',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')
+    ),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=0,               # include P2 (C2..C5 -> P2..P6)
+        add_extra_convs='on_input',
+        num_outs=5
+    ),
+    bbox_head=dict(
+        type='RotatedRetinaHead',
+        num_classes=1,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='RotatedAnchorGenerator',
+            octave_base_scale=1.5,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],  # conservative anchors (helps precision)
+            strides=[4, 8, 16, 32, 64]  # P2..P6
+        ),
+        bbox_coder=dict(
+            type='DeltaXYWHAOBBoxCoder',
+            angle_range=angle_version,
+            norm_factor=None,
+            edge_swap=True,
+            proj_xy=True,
+            target_means=(.0, .0, .0, .0, .0),
+            target_stds=(1.0, 1.0, 1.0, 1.0, 1.0)
+        ),
+        # Precision-leaning focal loss
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.25,   # ↑ penalize low-score errors a bit more
+            alpha=0.20,   # ↓ slightly higher weight on negatives (1-alpha)
+            loss_weight=1.0
+        ),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)
+    ),
+    # Slightly stricter assigner to avoid borderline positives
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.55,   # was 0.50
+            neg_iou_thr=0.45,   # was 0.40
+            min_pos_iou=0,
+            ignore_iof_thr=-1,
+            iou_calculator=dict(type='RBboxOverlaps2D')
+        ),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False
+    ),
+    # Inference: rotated NMS + low score_thr to keep proposals (deployment will set higher post-thr)
+    test_cfg=dict(
+        nms_pre=2000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='rnms', iou_thr=0.12),  # use 'iou_threshold' if your version expects that key
+        max_per_img=1000
+    )
+)
+
+# -----------------------------
+# Dataset
+# -----------------------------
+dataset_type = 'DOTADataset'
+data_root = 'data/'
+classes = ('sperm',)
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True
+)
+
+# -----------------------------
+# Pipelines
+# -----------------------------
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(320, 320)),
+    dict(type='PhotoMetricDistortion'),  # mild photometric aug to cut background-triggered FPs
+    dict(
+        type='RRandomFlip',
+        flip_ratio=[0.25, 0.25, 0.25],
+        direction=['horizontal', 'vertical', 'diagonal'],
+        version=angle_version
+    ),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
+# Test-time flips (TTA) kept on; size stays 320
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(320, 320),
+        flip=True,  # enable TTA flips
+        transforms=[
+            dict(type='Resize'),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ]
+    )
+]
+
+print("✔ Precision-tuned config: stricter assigner + focal, photometric aug, TTA flips, rotated NMS (320×320).")
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'train/annfiles/',
+        img_prefix=data_root + 'train/images/',
+        pipeline=train_pipeline,
+        version=angle_version,
+        classes=classes
+    ),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'val/annfiles/',
+        img_prefix=data_root + 'val/images/',
+        pipeline=test_pipeline,
+        version=angle_version,
+        classes=classes
+    ),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'val/annfiles/',
+        img_prefix=data_root + 'val/images/',
+        pipeline=test_pipeline,
+        version=angle_version,
+        classes=classes
+    )
+)
+
+# -----------------------------
+# Evaluation
+# -----------------------------
+val_evaluator = dict(
+    type='DOTAMetric',
+    metric='mAP',
+    fthrs=[0.5]
+)
+evaluation = dict(interval=1, metric='mAP', save_best='mAP', rule='greater')
+
+# -----------------------------
+# Optimizer & LR
+# -----------------------------
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+
+lr_config = dict(
+    policy='CosineAnnealing',
+    min_lr=1e-6,
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001
+)
+
+total_epochs = 300
+runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
+
+# -----------------------------
+# Logging & Checkpoints
+# -----------------------------
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ]
+)
+
+checkpoint_config = dict(interval=1, max_keep_ckpts=3)
+
+custom_hooks = [
+    dict(type='NumClassCheckHook')
+    # If available in your stack, you can add EMA for stability:
+    # dict(type='EMAHook', momentum=0.0002, priority='ABOVE_NORMAL')
+]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+
+# -----------------------------
+# Init / resume
+# -----------------------------
+# For a clean precision-oriented retrain, you can start fresh:
+load_from = './work_dirs/my_rotated_retinanet_p2_only/best_mAP_epoch_32.pth'  # or point to your previous best to fine-tune instead of from-scratch
+resume_from = None
+
+workflow = [('train', 1)]
+work_dir = './work_dirs/my_rotated_retinanet_p2'
+
+
+
